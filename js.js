8c44a179fe0014f6d0706a996b633d0ec79ca262
raulcamunas <raulcamunas369@gmail.com>
2024-01-15 21:12:44 +0100

    Iteracion #4

--- js.js
+++ js.js
@@ -43,3 +43,25 @@ let o = p + j
 let c = 10
 let m = 5
 let i = c * m
+
+// EJERCICIO 4
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+avengers[0] = "IRON MAN";
+
+console.log(avengers.length);
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+
+rickAndMortyCharacters.pop
+console.log(rickAndMortyCharacters[0])
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+rickAndMortyCharacters.splice(1, 1);
+console.log(rickAndMortyCharacters);
+
