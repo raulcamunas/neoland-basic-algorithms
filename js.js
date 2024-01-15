0edaa9d62952873d83a2238de3915c8f1871e402
raulcamunas <raulcamunas369@gmail.com>
2024-01-15 20:39:41 +0100

    Iteraction#2

--- js.js
+++ js.js
@@ -1,5 +1,33 @@
-let myFavoriteHero = "Hulk"
-let x = 50
-let h = 5
-let y = 10
-let z = h + y
+// EJERCICIO 1
+
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+// EJERCICIO 2
+const character = {name: 'Jack Sparrow', age: 25};
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} y me gustan los lobos.`)
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+const sumaPrecios = toy1.price + toy2.price;
+
+console.log(`La suma de los precios de los juguetes es: ${sumaPrecios}`);
+
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000
+car1.finalPrice = car1.basePrice + globalBasePrice
+car2.finalPrice = car1.basePrice + globalBasePrice
+
+// EJERCICIO 3
