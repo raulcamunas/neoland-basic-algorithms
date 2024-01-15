262c46600874b6fcbc2c50772931121bc1223ec0
raulcamunas <raulcamunas369@gmail.com>
2024-01-15 20:48:34 +0100

    Iteracion#3

--- js.js
+++ js.js
@@ -31,3 +31,15 @@ car1.finalPrice = car1.basePrice + globalBasePrice
 car2.finalPrice = car1.basePrice + globalBasePrice
 
 // EJERCICIO 3
+
+console.log(10*5)
+console.log(10/2)
+console.log(15%9)
+
+let p = 10
+let j = 5
+let o = p + j
+
+let c = 10
+let m = 5
+let i = c * m
